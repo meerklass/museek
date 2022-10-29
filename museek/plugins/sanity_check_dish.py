2ea549b1b904e6c2e5cbc19ef0559e7b0501d4cc
amadeus <amsl@gmx.at>
2022-10-29 21:23:03 +0100

    First example plugins demonstrating the framework, to be expanded later

---
+++ museek/plugins/sanity_check_dish.py
@@ -0,0 +1,24 @@
+from ivy.plugin.abstract_plugin import AbstractPlugin
+
+from museek.plugins.sanity_check_observation import Plugin as SanityCheckObservationPlugin
+
+
+class Plugin(AbstractPlugin):
+    """
+    DOC
+    """
+
+    @classmethod
+    @property
+    def plugin_name(cls):
+        return 'SanityCheckDish'
+
+    def run(self):
+        print(self)
+        sanity_check_observation_results = self.output_of_plugin(SanityCheckObservationPlugin)
+
+        print(self.config.second_parameter)
+        second_result = self.config.second_parameter * 3
+        self.save_to_context({'result': second_result})
+        print(f'successfully accessed result of previous plugin: '
+              f'{sanity_check_observation_results.first_result}')
