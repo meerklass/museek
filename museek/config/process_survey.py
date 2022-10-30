5684370218695ad74c39b552788217bc307f9608
amadeus <amsl@gmx.at>
2022-10-30 15:23:02 +0000

    new example config

--- museek/config/process_survey.py
+++ museek/config/process_survey.py
@@ -1,19 +1,14 @@
-from enum import Enum
-
-
-class Pipeline(Enum):
-    """ Settings relating to the pipeline. """
-    plugins = [
+Pipeline = dict(
+    plugins=[
         'museek.plugins.sanity_check_observation_plugin',
         'museek.plugins.sanity_check_dish_plugin'
     ]
+)
 
+SanityCheckObservationPlugin = dict(
+    first_parameter=1.0  # description
+)
 
-class SanityCheckObservationPlugin(Enum):
-    """ Sanity check of an observation with large amount of output plots and checks for scanning errors. """
-    first_parameter = 1.0  # description
-
-
-class SanityCheckDishPlugin(Enum):
-    """ Sanity check of individual dishes and identification of defects. Many output plots. """
-    second_parameter = 2.0  # description
+SanityCheckDishPlugin = dict(
+    second_parameter=2.0  # description
+)
