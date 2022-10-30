61ebcd546d3b6ddd3b640dd50e67569250da4ab1
amadeus <amsl@gmx.at>
2022-10-30 15:29:14 +0000

    introduce new config_section.py class

--- museek/config/process_survey.py
+++ museek/config/process_survey.py
@@ -1,14 +1,16 @@
-Pipeline = dict(
+from ivy.utils.config_section import ConfigSection
+
+Pipeline = ConfigSection(
     plugins=[
         'museek.plugins.sanity_check_observation_plugin',
         'museek.plugins.sanity_check_dish_plugin'
     ]
 )
 
-SanityCheckObservationPlugin = dict(
+SanityCheckObservationPlugin = ConfigSection(
     first_parameter=1.0  # description
 )
 
-SanityCheckDishPlugin = dict(
+SanityCheckDishPlugin = ConfigSection(
     second_parameter=2.0  # description
 )
