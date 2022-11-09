3876774dc72b9faa3fcf4fb4d7b59c3d064a827d
amadeus <amsl@gmx.at>
2022-11-09 07:59:36 +0000

    helper class to contain receiver info

---
+++ museek/receiver.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class Polarisation(Enum):
+    """ Enum helper to contain the two polarisations"""
+    v = 'v'
+    h = 'h'
+
+
+class Receiver:
+    """ Helper class to contain receiver related infos. """
+
+    def __init__(self, antenna_number: int, polarisation: Polarisation):
+        """ Initializes a `Receiver` with the `antenna_index` of the dish and `polarisation`. """
+        self.antenna_number = antenna_number
+        self.antenna_name = f'm{self.antenna_number:03d}'
+        self._polarisation_enum = polarisation
+        self.polarisation = self._polarisation_enum.name
+
+    def __str__(self):
+        return f'{self.antenna_name}{self.polarisation}'
+
+    @classmethod
+    def from_string(cls, receiver_string: str):
+        """
+        Initializes a `Receiver` starting with an identifying string, e.g. 'm063v'.
+        :raise ValueError: if the input `receiver_string` does not conform
+        """
+        if (not receiver_string.startswith('m')
+                or not len(receiver_string) == 5
+                or not receiver_string[-1] in ['h', 'v']):
+            raise ValueError(f'Input `receiver_string` needs to be like e.g. "m063v", got {receiver_string}.')
+        return cls(antenna_number=int(receiver_string[1:-1]), polarisation=Polarisation(receiver_string[-1]))
---
+++ test/museek/test_receiver.py
@@ -0,0 +1,29 @@
+import unittest
+
+from museek.receiver import Receiver, Polarisation
+
+
+class TestReceiver(unittest.TestCase):
+
+    def test_str(self):
+        receiver = Receiver(antenna_number=0, polarisation=Polarisation.v)
+        self.assertEqual(str(receiver), 'm000v')
+
+    def test_from_string(self):
+        receiver = Receiver.from_string(receiver_string='m000v')
+        self.assertEqual(receiver.polarisation, Polarisation.v.name)
+        self.assertEqual(receiver.antenna_number, 0)
+        self.assertEqual(receiver.antenna_name, 'm000')
+
+    def test_from_string_when_invalid_polarisation_expect_raise(self):
+        self.assertRaises(ValueError, Receiver.from_string, receiver_string='m000r')
+
+    def test_from_string_when_invalid_antenna_index_expect_raise(self):
+        self.assertRaises(ValueError, Receiver.from_string, receiver_string='m1v')
+
+    def test_from_string_when_invalid_receiver_string_expect_raise(self):
+        self.assertRaises(ValueError, Receiver.from_string, receiver_string='r001v')
+
+
+if __name__ == '__main__':
+    unittest.main()
