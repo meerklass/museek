612b77bcb186014a085513fcd7f1d5632fb6d86a
amadeus <amsl@gmx.at>
2022-11-15 15:08:18 +0000

    demo integration test

---
+++ test/museek/plugins/demo/test_demo_plugins.py
@@ -0,0 +1,10 @@
+import unittest
+
+from ivory.cli.main import _main
+
+
+class TestDemoPlugins(unittest.TestCase):
+    def test_demo_plugs(self):
+        self.assertIsNone(_main('--DemoPlotPlugin-do-show=False',
+                                '--DemoPlotPlugin-do-save=False',
+                                'museek.config.demo'))
