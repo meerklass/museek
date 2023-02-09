39cabd37a59284998d0d41561a95cfc860e467b0
amadeus <amsl@gmx.at>
2023-02-09 09:12:40 +0000

    play

--- museek/config/process_data.py
+++ museek/config/process_data.py
@@ -8,10 +8,9 @@ Pipeline = ConfigSection(
 )
 
 InOutPlugin = ConfigSection(
-    block_name='1631379874',  # observation time stamp
+    # block_name='1631379874',  # observation time stamp
     # block_name='1632184922',  # observation time stamp
-    # block_name='1631552188',  # observation time stamp   DATA NOT DOWNLOADED PROPERLY
-    # block_name='1632760885',  # observation time stamp  DATA NOT DOWNLOADED PROPERLY
+    block_name='1632760885',  # observation time stamp
     receiver_list=['m000h', 'm000v', 'm007v','m007h'],
     token=None,  # archive token
     data_folder='/idia/projects/hi_im/SCI-20210212-MS-01/',  # only relevant if `token` is `None`
@@ -30,7 +29,6 @@ ZebraRemoverPlugin = ConfigSection(
         '1631379874': (1500, 'end'),
         '1632184922': (0, 500),
         '1632760885': (1500, 'end'),
-        '1631552188': (0, 'end'),
         '1638898468': (0, 1500),
         '1637354605': (0, 1500),
         '1631667564': (2000, 'end')
