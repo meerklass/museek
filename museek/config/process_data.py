2ec8ee1b60d6a1e2bd17d3b2dea5c87e9de70ed4
amadeus <amsl@gmx.at>
2023-03-02 15:10:08 +0000

    update config

--- museek/config/process_data.py
+++ museek/config/process_data.py
@@ -3,15 +3,26 @@ from ivory.utils.config_section import ConfigSection
 Pipeline = ConfigSection(
     plugins=[
         'museek.plugin.in_plugin',
-        'museek.plugin.out_plugin',
+        # 'museek.plugin.out_plugin',
         # 'museek.plugin.zebra_remover_plugin',
-        'museek.plugin.bandpass_plugin'
+        # 'museek.plugin.bandpass_plugin'
     ]
 )
 
 InPlugin = ConfigSection(
     block_name='1631379874',  # observation time stamp
-    receiver_list=['m000h'],
+    receiver_list=['m000h',
+                   'm000v',
+                   'm008h',
+                   'm008v',
+                   'm012h',
+                   'm012v',
+                   'm014h',
+                   'm014v',
+                   'm019h',
+                   'm019v',
+                   'm028h',
+                   'm028v'],
     token=None,  # archive token
     data_folder='/idia/projects/hi_im/SCI-20210212-MS-01/',  # only relevant if `token` is `None`
     force_load_from_correlator_data=False,  # if `True`, the local `cache` folder is ignored
