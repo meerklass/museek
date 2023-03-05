e707af06fa5e429e8fa5c54f4bd44bcb94673e61
amadeus <amsl@gmx.at>
2023-03-05 12:04:43 +0000

    config file update

--- museek/config/process_data.py
+++ museek/config/process_data.py
@@ -1,29 +1,39 @@
+import os
+
+from definitions import ROOT_DIR
 from ivory.utils.config_section import ConfigSection
 
 Pipeline = ConfigSection(
     plugins=[
         'museek.plugin.in_plugin',
         'museek.plugin.out_plugin',
+        'museek.plugin.point_source_flagger_plugin',
         'museek.plugin.zebra_remover_plugin',
     ]
 )
 
 InPlugin = ConfigSection(
     block_name='1631379874',  # observation time stamp
-    receiver_list=['m000h'],
+    receiver_list=['m000h', 'm008h', 'm028v'],
     token=None,  # archive token
     data_folder='/idia/projects/hi_im/SCI-20210212-MS-01/',  # only relevant if `token` is `None`
     force_load_from_correlator_data=False,  # if `True`, the local `cache` folder is ignored
     # if `True`, the extracted visibilities, flags and weights are stored to disc for quicker access
     do_save_visibility_to_disc=True,
     do_use_noise_diode=True,
-    do_store_context=True,
+    do_store_context=False,
     context_folder=None,  # directory to store results, if `None`, a 'results/' is chosen
 )
 
 OutPlugin = ConfigSection(
-    output_folder = None  # this means default location is chosen
+    output_folder=None  # this means default location is chosen
 )
+
+PointSourceFlaggerPlugin = ConfigSection(
+    point_source_file_path=os.path.join(ROOT_DIR, 'data/radio_point_sources.txt'),
+    angle_threshold=0.5
+)
+
 
 ZebraRemoverPlugin = ConfigSection(
     reference_channel=3000,
