ba0db54fdf44fb901d1a91dcca85c2f00b4180fe
amadeus <amsl@gmx.at>
2023-04-19 10:40:33 +0100

    new plugin for rfi flagging and config update

--- museek/config/process_data.py
+++ museek/config/process_data.py
@@ -58,6 +58,12 @@ ZebraRemoverPlugin = ConfigSection(
 )
 
 AoflaggerPlugin = ConfigSection(
+    first_threshold=0.1,  # First threshold value
+    threshold_scales=[0.5, 0.55, 0.62, 0.75, 1],
+    smoothing_kernel=(20, 40),  # Smoothing, kernel window size in time and frequency axis
+    smoothing_sigma=(7.5, 15),  # Smoothing, kernel sigma in time and frequency axis
+    struct_size=(6, 1),  # size of struct for dilation in time and frequency direction [pixels]
+    channel_flag_threshold=0.6,
     flag_combination_threshold=1
 )
 
--- museek/plugin/aoflagger_plugin.py
+++ museek/plugin/aoflagger_plugin.py
@@ -1,70 +1,108 @@
+import os
+
+from matplotlib import pyplot as plt
+
 from ivory.plugin.abstract_plugin import AbstractPlugin
 from ivory.utils.requirement import Requirement
+from ivory.utils.result import Result
 from museek.data_element import DataElement
 from museek.enum.result_enum import ResultEnum
+from museek.factory.data_element_factory import DataElementFactory
 from museek.flag_element import FlagElement
+from museek.flag_factory import FlagFactory
 from museek.rfi_mitigation.aoflagger import get_rfi_mask
+from museek.rfi_mitigation.rfi_post_process import RfiPostProcess
 from museek.time_ordered_data import TimeOrderedData
 from museek.visualiser import waterfall
-from matplotlib import pyplot as plt
 
 
 class AoflaggerPlugin(AbstractPlugin):
-    """ Plugin to calculate RFI flags using the aoflagger algorithm. """
+    """ Plugin to calculate RFI flags using the aoflagger algorithm and to post-process them. """
 
-    def __init__(self, flag_combination_threshold: int):
+    def __init__(self,
+                 first_threshold: float,
+                 threshold_scales: list[float],
+                 smoothing_kernel: tuple[int, int],
+                 smoothing_sigma: tuple[float, float],
+                 struct_size: tuple[int, int] | None,
+                 channel_flag_threshold: float,
+                 flag_combination_threshold: int):
         """
+        Initialise the plugin
+        :param first_threshold: initial threshold to be used for the aoflagger algorithm
+        :param threshold_scales: list of sensitivities
+        :param smoothing_kernel: smoothing kernel window size tuple for axes 0 and 1
+        :param smoothing_sigma: smoothing kernel sigma tuple for axes 0 and 1
+        :param struct_size: structure size for binary dilation, closing etc
+        :param channel_flag_threshold: if the fraction of flagged channels exceeds this, all channels are flagged
+        :param flag_combination_threshold: for combining sets of flags, usually `1`
         """
-        self.flag_combination_threshold = flag_combination_threshold
         super().__init__()
+        self.first_threshold = first_threshold
+        self.threshold_scales = threshold_scales
+        self.smoothing_kernel = smoothing_kernel
+        self.smoothing_sigma = smoothing_sigma
+        self.struct_size = struct_size
+        self.flag_combination_threshold = flag_combination_threshold
+        self.data_element_factory = DataElementFactory()
+        self.channel_flag_threshold = channel_flag_threshold
 
     def set_requirements(self):
         """ Set the requirements. """
-        self.requirements = [Requirement(location=ResultEnum.SCAN_DATA, variable='scan_data')]
+        self.requirements = [Requirement(location=ResultEnum.SCAN_DATA, variable='scan_data'),
+                             Requirement(location=ResultEnum.OUTPUT_PATH, variable='output_path')]
 
-    def run(self, scan_data: TimeOrderedData):
-        """ """
+    def run(self, scan_data: TimeOrderedData, output_path: str):
+        """
+        Run the Aoflagger algorithm and post-process the result. Done for each receiver separately.
+        :param scan_data: time ordered data containing the observation's scanning part
+        :param output_path: path to store results
+        """
         scan_data.load_visibility_flags_weights()
         initial_flags = scan_data.flags.combine(threshold=self.flag_combination_threshold)
 
-        chi_1 = 50  # First threshold value
-        sm_kernel_m = 40  # Smoothing, kernel window size in axis=1
-        sm_kernel_n = 20  # Smoothing, kernel window size in axis=0
-        sm_sigma_m = 15  # Smoothing, kernel sigma in axis=1
-        sm_sigma_n = 7.5  # Smoothing, kernel sigma in axis=0
-
-        struct_size_0 = 1  # size of struct for dilation on freq direction [pixels]
-        struct_size_1 = 6  # size of struct for dilation on time direction [pixels]
-        di_kwargs = dict(struct_size_0=struct_size_0, struct_size_1=struct_size_1)
-        sm_kwargs = dict(M=sm_kernel_m, N=sm_kernel_n, sigma_m=sm_sigma_m, sigma_n=sm_sigma_n)
-
-        eta_i = [0.5, 0.55, 0.62, 0.75, 1]
-
-        # to speed up for testing
-        # times = range(500, 2500)
-        # freqs = range(800,2000)
         times = range(scan_data.visibility.shape[0])
         freqs = range(scan_data.visibility.shape[1])
 
-        waterfall(scan_data.visibility, scan_data.flags, cmap='gist_ncar', norm='log')
-        plt.show()
+        new_flag = FlagElement(flags=[FlagFactory().empty_flag(shape=scan_data.visibility.shape)])
 
         for i_receiver, receiver in enumerate(scan_data.receivers):
+            if not os.path.isdir(receiver_path := os.path.join(output_path, receiver.name)):
+                os.makedirs(receiver_path)
             visibility = scan_data.visibility.get(recv=i_receiver, time=times, freq=freqs)
             initial_flag = initial_flags.get(recv=i_receiver, time=times, freq=freqs)
-            rfi_flag = get_rfi_mask(tod=visibility,
+            rfi_flag = get_rfi_mask(time_ordered=visibility,
                                     mask=initial_flag,
-                                    first_threshold=chi_1,
-                                    threshold_scales=eta_i,
-                                    do_plot=False,
-                                    smoothing_kwargs=sm_kwargs,
-                                    dilation_kwargs=di_kwargs)
-            # scan_data.flags.add_flag(rfi_flag)
+                                    first_threshold=self.first_threshold,
+                                    threshold_scales=self.threshold_scales,
+                                    output_path=receiver_path,
+                                    smoothing_window_size=self.smoothing_kernel,
+                                    smoothing_sigma=self.smoothing_sigma)
+            rfi_flag = self.post_process_flag(flag=rfi_flag, initial_flag=initial_flag)
+            new_flag.insert_receiver_flag(flag=rfi_flag, i_receiver=i_receiver, index=0)
 
-            # new_flags = FlagElement(flags=[initial_flag])
-            # new_flags.add_flag(rfi_flag)
+        scan_data.flags.add_flag(flag=new_flag)
+        self.set_result(result=Result(location=ResultEnum.SCAN_DATA, result=scan_data, allow_overwrite=True))
 
-            new_flags = FlagElement(flags=[rfi_flag])
+        waterfall(scan_data.visibility.get(recv=0, time=times, freq=freqs),
+                  scan_data.flags.get(recv=0, time=times, freq=freqs),
+                  cmap='gist_ncar',
+                  norm='log')
+        plt.savefig(os.path.join(output_path, 'rfi_mitigation_result_receiver_0.png'))
 
-            waterfall(scan_data.visibility.get(recv=i_receiver, time=times, freq=freqs), new_flags, cmap='gist_ncar', norm='log')
-            plt.show()
+    def post_process_flag(self, flag: DataElement, initial_flag: DataElement) -> DataElement:
+        """
+        Post process `flag` and return the result.
+        The following is done:
+        - `flag` is dilated using `self.struct_size` if it is not `None`
+        - binary closure is applied to `flag`
+        - if a certain fraction of all channels is flagged at any timestamp, the remainder is flagged as well
+        :param flag: binary mask to be post-processed
+        :param initial_flag: initial flag on which `flag` was based
+        :return: the result of the post-processing, a binary mask
+        """
+        post_process = RfiPostProcess(new_flag=flag, initial_flag=initial_flag, struct_size=self.struct_size)
+        post_process.binary_mask_dilation()
+        post_process.binary_mask_closing()
+        post_process.flag_all_channels(channel_flag_threshold=self.channel_flag_threshold)
+        return post_process.get_flag()
