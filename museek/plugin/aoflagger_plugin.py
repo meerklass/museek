badeaecd4bad2c0e6811cf190aa8e9113bdd5cf1
amadeus <amsl@gmx.at>
2023-05-10 12:38:06 +0100

    aoflagger plugin updates

--- museek/plugin/aoflagger_plugin.py
+++ museek/plugin/aoflagger_plugin.py
@@ -26,7 +26,9 @@ class AoflaggerPlugin(AbstractPlugin):
                  smoothing_sigma: tuple[float, float],
                  struct_size: tuple[int, int] | None,
                  channel_flag_threshold: float,
-                 flag_combination_threshold: int):
+                 time_dump_flag_threshold: float,
+                 flag_combination_threshold: int,
+                 do_store_context: bool):
         """
         Initialise the plugin
         :param first_threshold: initial threshold to be used for the aoflagger algorithm
@@ -35,7 +37,9 @@ class AoflaggerPlugin(AbstractPlugin):
         :param smoothing_sigma: smoothing kernel sigma tuple for axes 0 and 1
         :param struct_size: structure size for binary dilation, closing etc
         :param channel_flag_threshold: if the fraction of flagged channels exceeds this, all channels are flagged
+        :param time_dump_flag_threshold: if the fraction of flagged time dumps exceeds this, all time dumps are flagged
         :param flag_combination_threshold: for combining sets of flags, usually `1`
+        :param do_store_context: if `True` the context is stored to disc after finishing the plugin
         """
         super().__init__()
         self.first_threshold = first_threshold
@@ -46,27 +50,34 @@ class AoflaggerPlugin(AbstractPlugin):
         self.flag_combination_threshold = flag_combination_threshold
         self.data_element_factory = DataElementFactory()
         self.channel_flag_threshold = channel_flag_threshold
+        self.time_dump_flag_threshold = time_dump_flag_threshold
+        self.do_store_context = do_store_context
 
     def set_requirements(self):
-        """ Set the requirements. """
-        self.requirements = [Requirement(location=ResultEnum.SCAN_DATA, variable='scan_data'),
-                             Requirement(location=ResultEnum.OUTPUT_PATH, variable='output_path')]
+        """ Set the requirements, the entire `data`, a path to store results and the name of the data block. """
+        self.requirements = [Requirement(location=ResultEnum.DATA, variable='data'),
+                             Requirement(location=ResultEnum.OUTPUT_PATH, variable='output_path'),
+                             Requirement(location=ResultEnum.BLOCK_NAME, variable='block_name')]
 
-    def run(self, scan_data: TimeOrderedData, output_path: str):
+    def run(self,
+            data: TimeOrderedData,
+            output_path: str,
+            block_name: str):
         """
         Run the Aoflagger algorithm and post-process the result. Done for each receiver separately.
-        :param scan_data: time ordered data containing the observation's scanning part
+        :param data: time ordered data containing the entire observation
         :param output_path: path to store results
+        :param block_name: name of the data block
         """
-        scan_data.load_visibility_flags_weights()
-        initial_flags = scan_data.flags.combine(threshold=self.flag_combination_threshold)
+        data.load_visibility_flags_weights()
+        initial_flags = data.flags.combine(threshold=self.flag_combination_threshold)
 
-        new_flag = FlagElement(flags=[FlagFactory().empty_flag(shape=scan_data.visibility.shape)])
+        new_flag = FlagElement(flags=[FlagFactory().empty_flag(shape=data.visibility.shape)])
 
-        for i_receiver, receiver in enumerate(scan_data.receivers):
+        for i_receiver, receiver in enumerate(data.receivers):
             if not os.path.isdir(receiver_path := os.path.join(output_path, receiver.name)):
                 os.makedirs(receiver_path)
-            visibility = scan_data.visibility.get(recv=i_receiver)
+            visibility = data.visibility.get(recv=i_receiver)
             initial_flag = initial_flags.get(recv=i_receiver)
             rfi_flag = get_rfi_mask(time_ordered=visibility,
                                     mask=initial_flag,
@@ -78,16 +89,28 @@ class AoflaggerPlugin(AbstractPlugin):
             rfi_flag = self.post_process_flag(flag=rfi_flag, initial_flag=initial_flag)
             new_flag.insert_receiver_flag(flag=rfi_flag, i_receiver=i_receiver, index=0)
 
-        scan_data.flags.add_flag(flag=new_flag)
-        self.set_result(result=Result(location=ResultEnum.SCAN_DATA, result=scan_data, allow_overwrite=True))
+        data.flags.add_flag(flag=new_flag)
 
-        waterfall(scan_data.visibility.get(recv=0),
-                  scan_data.flags.get(recv=0),
+        waterfall(data.visibility.get(recv=0),
+                  data.flags.get(recv=0),
                   cmap='gist_ncar',
                   norm='log')
-        plt.savefig(os.path.join(output_path, 'rfi_mitigation_result_receiver_0.png'))
+        plt.savefig(os.path.join(output_path, 'rfi_mitigation_result_receiver_0.png'), dpi=1000)
+        plt.close()
 
-    def post_process_flag(self, flag: DataElement, initial_flag: DataElement) -> DataElement:
+        self.set_result(result=Result(location=ResultEnum.DATA, result=data, allow_overwrite=True))
+        if self.do_store_context:
+            context_file_name = 'aoflagger_plugin.pickle'
+            context_folder = os.path.join(ROOT_DIR, 'results/')
+            context_directory = os.path.join(context_folder, f'{block_name}/')
+            self.store_context_to_disc(context_file_name=context_file_name,
+                                       context_directory=context_directory)
+
+    def post_process_flag(
+            self,
+            flag: DataElement,
+            initial_flag: DataElement
+    ) -> DataElement:
         """
         Post process `flag` and return the result.
         The following is done:
@@ -101,5 +124,13 @@ class AoflaggerPlugin(AbstractPlugin):
         post_process = RfiPostProcess(new_flag=flag, initial_flag=initial_flag, struct_size=self.struct_size)
         post_process.binary_mask_dilation()
         post_process.binary_mask_closing()
-        post_process.flag_all_channels(channel_flag_threshold=self.channel_flag_threshold)
-        return post_process.get_flag()
+        rfi_result = post_process.get_flag()
+        return rfi_result
+
+        # TODO: uncomment this after FlagElement has been created with the + functionality
+        # # operations on the entire mask
+        # post_process = RfiPostProcess(new_flag=rfi_result+initial_flag, initial_flag=None, struct_size=self.struct_size)
+        # post_process.flag_all_channels(channel_flag_threshold=self.channel_flag_threshold)
+        # post_process.flag_all_time_dumps(time_dump_flag_threshold=self.time_dump_flag_threshold)
+        # overall_result = post_process.get_flag()
+        # return rfi_result + overall_result
