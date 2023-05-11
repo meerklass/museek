dae94c53943aa84acfe3be9ad3d6cf0d50927c10
amadeus <amsl@gmx.at>
2023-05-11 10:40:44 +0100

    new demo plugin for parallelisation

--- museek/config/demo.py
+++ museek/config/demo.py
@@ -1,10 +1,12 @@
 from ivory.utils.config_section import ConfigSection
+from museek.enum.demo_enum import DemoEnum
 
 Pipeline = ConfigSection(
     plugins=[
         'museek.plugin.demo.demo_load_plugin',
         'museek.plugin.demo.demo_flip_plugin',
-        'museek.plugin.demo.demo_plot_plugin'
+        'museek.plugin.demo.demo_plot_plugin',
+        'museek.plugin.demo.demo_joblib_plugin'
     ],
 )
 
@@ -21,4 +23,10 @@ DemoPlotPlugin = ConfigSection(
 DemoFlipPlugin = ConfigSection(
     do_flip_right_left=True,
     do_flip_top_bottom=True
+)
+
+DemoJoblibPlugin = ConfigSection(
+    n_iter=10,
+    n_jobs=2,
+    verbose=0
 )
