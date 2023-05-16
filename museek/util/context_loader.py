81a89f453952f96124f277f1887870dd5e76bcfc
amadeus <amsl@gmx.at>
2023-05-16 17:19:37 +0100

    new class to access the context files

---
+++ museek/util/context_loader.py
@@ -0,0 +1,30 @@
+import pickle
+from enum import Enum
+from typing import Any
+
+from ivory.plugin.abstract_plugin import AbstractPlugin
+
+
+class ContextLoader:
+    """ Class to load a context file externally. """
+
+    def __init__(self, context_path: str):
+        """ Initialise with the path to the context pickle `context_path`. """
+        self._context_path = context_path
+
+        with open(self._context_path, 'rb') as input_file:
+            self.context = pickle.load(input_file)
+
+    def get_result(self, location: Enum) -> Any:
+        """
+        Return the result stored in `self.context` under `location`.
+        Note: `self.context[location]` needs to be a `Result` object.
+        """
+        return self.context[location].result
+
+    def requirements_dict(self, plugin: AbstractPlugin) -> dict[str, Any]:
+        """ Return the requirements of `plugin` as a dictionary. """
+        result_dict = {}
+        for requirement in plugin.requirements:
+            result_dict[requirement.variable] = self.context[requirement.location].result
+        return result_dict
---
+++ test/museek/util/test_context_loader.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import patch, Mock
+
+from museek.util.context_loader import ContextLoader
+
+
+class TestContextLoader(unittest.TestCase):
+
+    @patch('museek.util.context_loader.open')
+    @patch('museek.util.context_loader.pickle')
+    def test_ini(self, mock_pickle, mock_open):
+        ContextLoader(context_path='context_path')
+        mock_pickle.load.assert_called_once_with(mock_open.return_value.__enter__.return_value)
+        mock_open.assert_called_once_with('context_path', 'rb')
+
+    @patch('museek.util.context_loader.open')
+    @patch('museek.util.context_loader.pickle')
+    def test_requirements_dict(self, mock_pickle, mock_open):
+        context_loader = ContextLoader(context_path='context_path')
+        mock_pickle.load.assert_called_once()
+        mock_open.assert_called_once()
+        mock_plugin = Mock(requirements=[Mock(), Mock()])
+        requirements_dict = context_loader.requirements_dict(plugin=mock_plugin)
+        expect = mock_pickle.load.return_value.__getitem__.return_value.result
+        self.assertEqual(requirements_dict[mock_plugin.requirements[0].variable], expect)
+        self.assertEqual(requirements_dict[mock_plugin.requirements[1].variable], expect)
