969cd8c30471e6dd4b8559c736576f6dd4b9db54
wkhu-astro <wkhu@nao.cas.cn>
2024-07-08 00:38:37 +0200

    add the aoflagger_postcalibration_plugin to mask wide, faint, and negative-peak RFIs

--- museek/plugin/aoflagger_postcalibration_plugin.py
+++ museek/plugin/aoflagger_postcalibration_plugin.py
@@ -12,35 +12,43 @@ from museek.enums.result_enum import ResultEnum
 from museek.flag_element import FlagElement
 from museek.flag_factory import FlagFactory
 from museek.rfi_mitigation.aoflagger import get_rfi_mask
+from museek.rfi_mitigation.aoflagger_1d import get_rfi_mask_1d
 from museek.rfi_mitigation.rfi_post_process import RfiPostProcess
 from museek.time_ordered_data import TimeOrderedData
 from museek.util.report_writer import ReportWriter
+from museek.util.tools import Synch_model_sm
 from museek.visualiser import waterfall
 from museek.util.tools import flag_percent_recv
 import pickle
 import numpy as np
+import scipy
 import datetime
 from scipy.stats import spearmanr
 import pysm3
 import pysm3.units as u
 import healpy as hp
 from astropy.coordinates import SkyCoord
+from scipy import ndimage
 
 
 class AoflaggerPostCalibrationPlugin(AbstractParallelJoblibPlugin):
     """ Plugin to calculate RFI flags using the aoflagger algorithm and to post-process them, for calibrated data """
 
     def __init__(self,
-                 mask_type: str,
-                 first_threshold: float,
+                 first_threshold_rms: float,
+                 first_threshold_flag_fraction: float,
                  threshold_scales: list[float],
-                 smoothing_kernel: tuple[int, int],
-                 smoothing_sigma: tuple[float, float],
+                 smoothing_kernel_rms: int,
+                 smoothing_sigma_rms: float,
+                 smoothing_kernel_flag_fraction: int,
+                 smoothing_sigma_flag_fraction: float,
                  struct_size: tuple[int, int] | None,
                  channel_flag_threshold: float,
                  time_dump_flag_threshold: float,
                  flag_combination_threshold: int,
-                 correlation_threshold: float,
+                 poly_fit_degree: int,
+                 poly_fit_threshold: float,
+                 correlation_threshold_ant: float,
                  synch_model:[str],
                  nside: int,
                  beamsize: float,
@@ -49,16 +57,20 @@ class AoflaggerPostCalibrationPlugin(AbstractParallelJoblibPlugin):
                  **kwargs):
         """
         Initialise the plugin
-        :param mask_type: the data to which the flagger will be applied
-        :param first_threshold: initial threshold to be used for the aoflagger algorithm
+        :param first_threshold_rms: initial threshold to be used for the aoflagger for the rms of powerlaw fitting residulas
+        :param first_threshold_flag_fraction: initial threshold to be used for the aoflagger for the flagged fraction
         :param threshold_scales: list of sensitivities
-        :param smoothing_kernel: smoothing kernel window size tuple for axes 0 and 1
-        :param smoothing_sigma: smoothing kernel sigma tuple for axes 0 and 1
+        :param smoothing_kernel_rms: smoothing kernel window size for axis 0, used by aoflagger on the RMS of power-law fitting residuals
+        :param smoothing_sigma_rms: smoothing kernel sigma for axes 0, used by aoflagger on the RMS of power-law fitting residuals
+        :param smoothing_kernel_flag_fraction: smoothing kernel window size for axes 0, used by aoflagger on the flagged fraction
+        :param smoothing_sigma_flag_fraction: smoothing kernel sigma for axes 0, used by aoflagger on the flagged fraction
         :param struct_size: structure size for binary dilation, closing etc
         :param channel_flag_threshold: if the fraction of flagged channels exceeds this, all channels are flagged
         :param time_dump_flag_threshold: if the fraction of flagged time dumps exceeds this, all time dumps are flagged
         :param flag_combination_threshold: for combining sets of flags, usually `1`
-        :param correlation_threshold: correlation coefficient threshold between calibrated data and synch model for excluding bad antennas
+        :param poly_fit_degree: degree of polynomials used to fit the data with the time median removed
+        :param poly_fit_threshold: threshold (times of MAD) of polynomials fitting flagging
+        :param correlation_threshold_ant: correlation coefficient threshold between calibrated data and synch model for excluding bad antennas
         :param synch_model: model used to create synchrotron sky
         :param nside: resolution parameter at which the synchrotron model is to be calculated
         :param beamsize: the beam fwhm used to smooth the Synch model [arcmin]
@@ -66,16 +78,20 @@ class AoflaggerPostCalibrationPlugin(AbstractParallelJoblibPlugin):
         :param do_store_context: if `True` the context is stored to disc after finishing the plugin
         """
         super().__init__(**kwargs)
-        self.mask_type = mask_type
-        self.first_threshold = first_threshold
+        self.first_threshold_rms = first_threshold_rms
+        self.first_threshold_flag_fraction = first_threshold_flag_fraction
         self.threshold_scales = threshold_scales
-        self.smoothing_kernel = smoothing_kernel
-        self.smoothing_sigma = smoothing_sigma
+        self.smoothing_kernel_rms = smoothing_kernel_rms
+        self.smoothing_sigma_rms = smoothing_sigma_rms
+        self.smoothing_kernel_flag_fraction = smoothing_kernel_flag_fraction
+        self.smoothing_sigma_flag_fraction = smoothing_sigma_flag_fraction
         self.struct_size = struct_size
         self.flag_combination_threshold = flag_combination_threshold
         self.channel_flag_threshold = channel_flag_threshold
         self.time_dump_flag_threshold = time_dump_flag_threshold
-        self.correlation_threshold = correlation_threshold
+        self.poly_fit_degree = poly_fit_degree
+        self.poly_fit_threshold = poly_fit_threshold
+        self.correlation_threshold_ant = correlation_threshold_ant
         self.synch_model = synch_model
         self.nside = nside
         self.beamsize = beamsize
@@ -101,7 +117,7 @@ class AoflaggerPostCalibrationPlugin(AbstractParallelJoblibPlugin):
             flag_report_writer: ReportWriter,
             output_path: str,
             block_name: str) \
-            -> Generator[tuple[str, DataElement, FlagElement, np.ndarray, np.ndarray, np.ndarray, np.ndarray, str], None, None]:
+            -> Generator[tuple[str, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, str, str], None, None]:
         """
         Yield a `tuple` of the results path for one antenna, the scanning calibrated data for one antenna and the flag for one antenna.
         :param scan_data: time ordered data containing the scanning part of the observation
@@ -111,33 +127,82 @@ class AoflaggerPostCalibrationPlugin(AbstractParallelJoblibPlugin):
         :param output_path: path to store results
         :param block_name: name of the data block, not used here but for setting results
         """
-        print(f'flag_antennas_using_correlation_with_synch_model: Producing synch sky: synch model {self.synch_model} used')
+        print(f'flag frequency and antennas usingcorrelation with synch model: Producing synch sky: synch model {self.synch_model} used')
+
         receiver_path = None
         for i_antenna, antenna in enumerate(scan_data.antennas):
-            visibility = calibrated_data.data[:,:,i_antenna]
-            initial_flag = calibrated_data.mask[:,:,i_antenna]
             right_ascension = scan_data.right_ascension.get(recv=i_antenna).squeeze
             declination = scan_data.declination.get(recv=i_antenna).squeeze
-            yield receiver_path, DataElement(array=visibility[:,:,np.newaxis]), FlagElement(array=initial_flag[:,:,np.newaxis]), right_ascension, declination, freq_select, antenna.name
+            yield receiver_path, calibrated_data.data[:,:,i_antenna], calibrated_data.mask[:,:,i_antenna], right_ascension, declination, freq_select, antenna.name
 
-    def run_job(self, anything: tuple[str, DataElement, FlagElement, np.ndarray, np.ndarray, np.ndarray, str]) -> np.ndarray:
+    def run_job(self, anything: tuple[str, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, str]) -> np.ndarray:
         """
         Run the Aoflagger algorithm and post-process the result. Done for one antenna at a time.
-        :param anything: `tuple` of the output path, calibrated data, initial flag, right ascension, declination, frequency and antenna name
+        :param anything: `tuple` of the output path, calibrated data, flag, right ascension, declination, frequency and antenna name
         :return: updated mask
         """
-        receiver_path, visibility, initial_flag, right_ascension, declination, freq_select, antenna = anything
-        rfi_flag = get_rfi_mask(time_ordered=visibility,
-                                mask=initial_flag,
-                                mask_type=self.mask_type,
-                                first_threshold=self.first_threshold,
+        receiver_path, visibility_recv, initial_flag_recv, right_ascension, declination, freq_select, antenna  = anything
+
+        ###########  fitting a powerlaw to the time median removed data and then mask outlier   ######## 
+        visibility_recv_masked = np.ma.masked_array(visibility_recv, mask=initial_flag_recv)
+        visibility_recv_masked = visibility_recv_masked - np.ma.median(visibility_recv_masked, axis=0)
+        for i in range(visibility_recv.shape[0]):
+            visibility_recv_masked.mask[i], p_fit = self.polynomial_flag_outlier(freq_select, visibility_recv_masked.data[i], visibility_recv_masked.mask[i], self.poly_fit_degree, self.poly_fit_threshold)
+
+        #############  second run  #########
+        visibility_recv_masked = np.ma.masked_array(visibility_recv, mask=visibility_recv_masked.mask)
+        visibility_recv_masked = visibility_recv_masked - np.ma.median(visibility_recv_masked, axis=0)
+        residulas = np.zeros_like(visibility_recv)
+        for i in range(visibility_recv.shape[0]):
+            visibility_recv_masked.mask[i], p_fit = self.polynomial_flag_outlier(freq_select, visibility_recv_masked.data[i], visibility_recv_masked.mask[i], self.poly_fit_degree, self.poly_fit_threshold)
+            if visibility_recv_masked.mask[i].all():
+                pass
+            else:
+                residulas[i] = visibility_recv_masked.data[i] - np.polyval(p_fit, freq_select) 
+        residulas = np.ma.masked_array(residulas, mask=visibility_recv_masked.mask)
+
+        ###############  aoflagger on the rms of powerlaw fitting residulas  #################
+        residulas_rms = np.ma.std(residulas, axis=0).data
+        residulas_rms_mask = np.ma.std(residulas, axis=0).mask
+
+        rfi_flag = get_rfi_mask_1d(time_ordered=DataElement(array=residulas_rms[:,np.newaxis,np.newaxis]),
+                                mask=FlagElement(array=residulas_rms_mask[:,np.newaxis,np.newaxis]),
+                                mask_type='vis',
+                                first_threshold=self.first_threshold_rms,
                                 threshold_scales=self.threshold_scales,
                                 output_path=receiver_path,
-                                smoothing_window_size=self.smoothing_kernel,
-                                smoothing_sigma=self.smoothing_sigma)
+                                smoothing_window_size=self.smoothing_kernel_rms,
+                                smoothing_sigma=self.smoothing_sigma_rms)
 
-        initial_flag = self.post_process_flag(flag=rfi_flag, initial_flag=initial_flag).squeeze
-        return self.flag_antennas_using_correlation_with_synch_model(visibility.squeeze, initial_flag, right_ascension, declination, freq_select, antenna) 
+
+        rfi_flag_tile = np.tile(rfi_flag.squeeze, (visibility_recv.shape[0], 1))
+        initial_flag_tile = np.tile(residulas_rms_mask, (visibility_recv.shape[0], 1))
+        initial_flag_post = self.post_process_flag(flag=FlagElement(array=rfi_flag_tile[:,:,np.newaxis]), initial_flag=FlagElement(array=initial_flag_tile[:,:,np.newaxis]))
+
+        initial_flag_postrms = initial_flag_post.squeeze + visibility_recv_masked.mask
+
+
+        ###############  aoflagger on the flagged fraction  #################
+        flag_fraction = np.mean(initial_flag_postrms>0, axis=0)
+        flag_fraction_mask = flag_fraction > self.time_dump_flag_threshold
+
+        rfi_flag = get_rfi_mask_1d(time_ordered=DataElement(array=flag_fraction[:,np.newaxis,np.newaxis]),
+                                mask=FlagElement(array=flag_fraction_mask[:,np.newaxis,np.newaxis]),
+                                mask_type='vis',
+                                first_threshold=self.first_threshold_flag_fraction,
+                                threshold_scales=self.threshold_scales,
+                                output_path=receiver_path,
+                                smoothing_window_size=self.smoothing_kernel_flag_fraction,
+                                smoothing_sigma=self.smoothing_sigma_flag_fraction)
+
+
+        rfi_flag_tile = np.tile(rfi_flag.squeeze, (visibility_recv.shape[0], 1))
+        initial_flag_tile = np.tile(flag_fraction_mask, (visibility_recv.shape[0], 1))
+        initial_flag_post = self.post_process_flag(flag=FlagElement(array=rfi_flag_tile[:,:,np.newaxis]), initial_flag=FlagElement(array=initial_flag_tile[:,:,np.newaxis]))
+
+        initial_flag_postflagfraction = initial_flag_post.squeeze + initial_flag_postrms
+
+        return self.flag_antennas_using_correlation_with_synch_model(visibility_recv, initial_flag_postflagfraction, right_ascension, declination, freq_select, antenna) 
 
     def gather_and_set_result(self,
                               result_list: list[np.ndarray],
@@ -159,14 +224,15 @@ class AoflaggerPostCalibrationPlugin(AbstractParallelJoblibPlugin):
 
         calibrated_data.mask = np.array(result_list).transpose(1, 2, 0)
 
-        ########  if a certain fraction of a frequency channel is flagged at any timestamp and antennas, the remainder is flagged as well
+        ##########  if a certain fraction of a frequency channel is flagged at any timestamp and antennas, the remainder is flagged as well
         good_antennas = [~calibrated_data[:,:,i_antenna].mask.all() for i_antenna, antenna in enumerate(scan_data.antennas)]
         for i_freq, freq in enumerate(freq_select):
-            if np.mean(calibrated_data.mask[:,i_freq,good_antennas]) > self.channel_flag_threshold:
+            if np.mean(calibrated_data.mask[:,i_freq,good_antennas]) > self.time_dump_flag_threshold:
                 calibrated_data.mask[:,i_freq,:] = True
             else:
                 pass
 
+        ##########   report of the flagging  
         flag_percent = []
         antennas_list = []
         for i_antenna, antenna in enumerate(scan_data.antennas):
@@ -177,6 +243,7 @@ class AoflaggerPostCalibrationPlugin(AbstractParallelJoblibPlugin):
         lines = ['...........................', 'Running AoflaggerPostCalibrationPlugin...Finished at ' + current_datetime.strftime("%Y-%m-%d %H:%M:%S"), 'The flag fraction for each antenna: '] + [f'{x}  {y}' for x, y in zip(antennas_list, flag_percent)]
         flag_report_writer.write_to_report(lines)
 
+        #########   save results 
         self.set_result(result=Result(location=ResultEnum.CALIBRATED_VIS, result=calibrated_data, allow_overwrite=True))
         if self.do_store_context:
             context_file_name = 'aoflagger_plugin_postcalibration.pickle'
@@ -204,6 +271,7 @@ class AoflaggerPostCalibrationPlugin(AbstractParallelJoblibPlugin):
         post_process.binary_mask_dilation()
         post_process.binary_mask_closing()
         rfi_result = post_process.get_flag()
+
 
         # operations on the entire mask
         post_process = RfiPostProcess(new_flag=rfi_result + initial_flag,
@@ -269,13 +337,57 @@ class AoflaggerPostCalibrationPlugin(AbstractParallelJoblibPlugin):
 
             spearman_corr, spearman_p = spearmanr(map_freqmedian.data[~map_freqmedian.mask], synch_I.data[~synch_I.mask])
 
-            if spearman_corr > self.correlation_threshold:
+            if spearman_corr > self.correlation_threshold_ant:
                 pass
             else:
                 mask[:] = True
                 print(f'antenna {antenna} is masked, Spearman correlation coefficient is '+str(round(spearman_corr,3)))
 
         return mask
+
+
+    def polynomial_flag_outlier(self, x, y, mask, degree, threshold):
+        """
+        fitting a powerlaw to the input data and mask outliers which deviate the powerlaw larger than `threshold' times median absolute deviation
+
+        Parameters:
+        x: x-coordinates of the sample points [array]
+        y: y-coordinates of the sample points [array]
+        mask: mask for the sample points [bool]
+        degree: Degree of the fitting polynomial [int]
+        threshold: threshold of the masking [float]
+
+        Returns:
+        bool array: updated mask
+        1darray: fitting polynomial coefficients
+        """
+        
+        initial_mask = mask.copy()
+        #struct = np.ones(self.struct_size[1], dtype=bool)
+        ########  mask the whole data if the flagged fraction is larger than 0.5
+        if np.mean(mask>0) > self.channel_flag_threshold:
+            initial_mask[:] = True
+            p_fit = None
+        else:
+           fit_x = x[~mask]
+           fit_y = y[~mask]
+
+           # Fit a polynomial
+           p_fit = np.polyfit(fit_x, fit_y, degree)
+           y_fit = np.polyval(p_fit, fit_x)
+
+           # Calculate residuals
+           residuals = fit_y - y_fit
+           mad_residuals = scipy.stats.median_abs_deviation(residuals)
+
+           # Identify outliers
+           initial_mask[~mask] |= np.abs(residuals) >= threshold * mad_residuals
+
+           #to_dilate = initial_mask ^ mask
+           #dilated = ndimage.binary_dilation(to_dilate, structure=struct, iterations=1)
+           #initial_mask = initial_mask + dilated
+
+        return initial_mask, p_fit
 
 
 
