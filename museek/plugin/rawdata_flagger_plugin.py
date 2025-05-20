fcfdcfd94d51cd4d5b7bdae75d72101280a33ea7
wkhu-astro <wkhu@nao.cas.cn>
2025-05-20 08:12:31 +0200

    add flag_name_list

--- museek/plugin/rawdata_flagger_plugin.py
+++ museek/plugin/rawdata_flagger_plugin.py
@@ -37,21 +37,25 @@ class RawdataFlaggerPlugin(AbstractPlugin):
         """ Set the requirements. """
         self.requirements = [Requirement(location=ResultEnum.DATA, variable='data'),
                              Requirement(location=ResultEnum.FLAG_REPORT_WRITER, variable='flag_report_writer'),
-                             Requirement(location=ResultEnum.OUTPUT_PATH, variable='output_path')]
+                             Requirement(location=ResultEnum.OUTPUT_PATH, variable='output_path'),
+                             Requirement(location=ResultEnum.FLAG_NAME_LIST, variable='flag_name_list')]
 
-    def run(self, data: TimeOrderedData, flag_report_writer: ReportWriter, output_path: str):
+    def run(self, data: TimeOrderedData, flag_report_writer: ReportWriter, output_path: str, flag_name_list:list):
         """
         Flag raw data with values below a minimum
         :param data: time ordered data of the entire block
         :param flag_report_writer: report of the flag
         :param output_path: path to store results
+        :param flag_name_list: list of the name of existing flags
         """
         data.load_visibility_flags_weights(polars='auto')
         new_flag = np.zeros(data.visibility.shape, dtype=bool)
         new_flag[data.visibility.array < self.flag_lower_threshold] = True
 
         data.flags.add_flag(flag=FlagList.from_array(array=new_flag, element_factory=self.data_element_factory))
+        flag_name_list.append('rawdata_low_value')
         self.set_result(result=Result(location=ResultEnum.DATA, result=data, allow_overwrite=True))
+        self.set_result(result=Result(location=ResultEnum.FLAG_NAME_LIST, result=flag_name_list, allow_overwrite=True))
 
         if self.do_store_context:
             context_file_name = 'rawdata_flagger_plugin.pickle'
