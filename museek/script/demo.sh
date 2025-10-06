7f54df85493e6d468b86adcfac1b90477533f8e4
Piyanat Kittiwisit <piyanat.kittiwisit@gmail.com>
2025-10-06 15:36:53 +0200

    Add demo.sh example script

---
+++ museek/script/demo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name='MuSEEK-demo'
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --mem=4GB
+#SBATCH --output=museek-stdout.log
+#SBATCH --error=museek-stderr.log
+#SBATCH --time=00:05:00
+
+source /path/to/virtualenv/museek/bin/activate
+echo "Using a Python virtual environment: $(which)"
+
+echo "Submitting Slurm job"
+python museek museek.config.demo
