meerklass/museek

commit b554922386fbcc51f252f7baf5c2ce734855ea7e

Uncommented the lines in the plugin about the flag reporting to avoid compatibility issues. But note that this will give an error if no flagging is done prior to it.

museek/plugin/scan_track_split_plugin.py
@@ -67,11 +67,11 @@ class ScanTrackSplitPlugin(AbstractPlugin):
                                       allow_overwrite=False))
 
         
-        #receivers_list, flag_percent = flag_percent_recv(scan_data)
-        #branch, commit = git_version_info()
-        #current_datetime = datetime.datetime.now()
-        #lines = ['...........................', 'Running ScanTrackSplitPlugin with '+f"MuSEEK version: {branch} ({commit})", 'Finished at ' + current_datetime.strftime("%Y-%m-%d %H:%M:%S"), 'The flag fraction for each receiver: '] + [f'{x}  {y}' for x, y in zip(receivers_list, flag_percent)]
-        #flag_report_writer.write_to_report(lines)
+        receivers_list, flag_percent = flag_percent_recv(scan_data)
+        branch, commit = git_version_info()
+        current_datetime = datetime.datetime.now()
+        lines = ['...........................', 'Running ScanTrackSplitPlugin with '+f"MuSEEK version: {branch} ({commit})", 'Finished at ' + current_datetime.strftime("%Y-%m-%d %H:%M:%S"), 'The flag fraction for each receiver: '] + [f'{x}  {y}' for x, y in zip(receivers_list, flag_percent)]
+        flag_report_writer.write_to_report(lines)
 
         if self.do_store_context:
             context_file_name = 'scan_track_split_plugin.pickle'

commit 1b7f19f04c346f256b23d6163b93ac5e9be23fd5

fix: bug introduced in b554922386fbcc51f252f7baf5c2ce734855ea7e

museek/plugin/scan_track_split_plugin.py
@@ -98,17 +98,19 @@ class ScanTrackSplitPlugin(AbstractPlugin):
             )
         )
 
-        receivers_list, flag_percent = flag_percent_recv(scan_data)
-        branch, commit = git_version_info()
-        current_datetime = datetime.datetime.now()
-        lines = [
-            "...........................",
-            "Running ScanTrackSplitPlugin with "
-            + f"MuSEEK version: {branch} ({commit})",
-            "Finished at " + current_datetime.strftime("%Y-%m-%d %H:%M:%S"),
-            "The flag fraction for each receiver: ",
-        ] + [f"{x}  {y}" for x, y in zip(receivers_list, flag_percent)]
-        flag_report_writer.write_to_report(lines)
+        # Make flagging report if the data has been flagged
+        if scan_data.flags is not None:
+            receivers_list, flag_percent = flag_percent_recv(scan_data)
+            branch, commit = git_version_info()
+            current_datetime = datetime.datetime.now()
+            lines = [
+                "...........................",
+                "Running ScanTrackSplitPlugin with "
+                + f"MuSEEK version: {branch} ({commit})",
+                "Finished at " + current_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                "The flag fraction for each receiver: ",
+            ] + [f"{x}  {y}" for x, y in zip(receivers_list, flag_percent)]
+            flag_report_writer.write_to_report(lines)
 
         if self.do_store_context:
             context_file_name = "scan_track_split_plugin.pickle"